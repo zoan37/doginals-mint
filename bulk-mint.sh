ac53597f6c2bf6fb3f6898cfba33a12b60dcd2bb
Andrew M <35627271+zachzwei@users.noreply.github.com>
2024-02-17 19:30:19 +0800

    Update bulk-mint.sh

--- bulk-mint.sh
+++ bulk-mint.sh
@@ -13,9 +13,9 @@ target_address=$2
 token_name=$3
 while [ $count -lt $max_count ]; do
     echo "Current count: $count"
-    node . drc-20 mint "$target_address" "$token_name" 100000000
+    node . drc-20 mint "$target_address" "$token_name" 1000 12
     remaining=$((max_count - count))
     echo "Counts left: $remaining"
-    sleep 120  # Sleep for 3,5 minutes
+    sleep 200  # Sleep for 3,5 minutes
     ((count++))
 done
